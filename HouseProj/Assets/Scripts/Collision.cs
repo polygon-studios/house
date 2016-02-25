cc90e20c637161c736cdf382766f0f5c1a306553
unknown <s.tegan@hotmail.com>
2016-02-24 22:49:54 -0500

    house change from int to ext on button press L and K

--- HouseProj/Assets/Scripts/Collision.cs
+++ HouseProj/Assets/Scripts/Collision.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Collision : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
---
+++ HouseProj/Assets/Scripts/houseExt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class houseExt : MonoBehaviour {
+
+	SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.K)){
+			renderer.color = new Color(1f, 1f, 1f, 0f);
+		}
+		if(Input.GetKey(KeyCode.L)){
+			renderer.color = new Color(1f, 1f, 1f, 1f);
+		}
+	}
+}
