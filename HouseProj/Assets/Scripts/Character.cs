d7c499207d8c0e05f3804057a27cd7f68d729731
unknown <s.tegan@hotmail.com>
2016-03-03 13:01:16 -0500

    Walking in house on key press

--- HouseProj/Assets/Scripts/Character.cs
+++ HouseProj/Assets/Scripts/Character.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Character : MonoBehaviour {
 	public bool onGround = false;
@@ -9,7 +10,20 @@ public class Character : MonoBehaviour {
 	}
 	public KeyCode inputLeft;
 	public KeyCode inputRight;
-	
+	public string charID;
+
+	float itemDroppingTimer = 10;
+
+	bool startingPosSetLR = false;
+	bool startingPosSetRL = false;
+	bool startingPosSetLRB = false;
+
+	bool leftToRightPlaying = false;
+	bool rightToLeftPlaying = false;
+	bool leftToRightBINDLEPlaying = false;
+
+	List<Vector3> leftToRightPoints;
+	List<Vector3> leftToRightBINDLEPoints;
 
 	Animator animator;
 	public float speed; //walk 0.5f //run 0.8f
@@ -18,7 +32,7 @@ public class Character : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		animator = this.gameObject.GetComponent<Animator> ();
-
+		updateAnimationPoints ();
 	}
 	
 	// Update is called once per frame
@@ -27,16 +41,69 @@ public class Character : MonoBehaviour {
 	}
 
 	void Movement(){
-		/*
-		A button used to jump
-		X button used for picking up, activating, and using items
-		B button used to drop items
-		*/
-
 		animator.SetFloat ("foxMoveSpeed", Mathf.Abs (Input.GetAxis ("Horizontal")));
        
+		if(Input.GetKey(KeyCode.J) || leftToRightPlaying == true){
+			if(startingPosSetLR == false){
+				this.transform.position = leftToRightPoints[0];
+				startingPosSetLR = true;
+				transform.eulerAngles = new Vector2(0, 0);
+			}
+			leftToRightPlaying=true;
+			transform.position = Vector3.MoveTowards(this.transform.position, leftToRightPoints[1], Time.deltaTime*8f);
 
-		if (Input.GetKey (inputRight)) { //moving character right
+			if(transform.position == leftToRightPoints[1]){
+				startingPosSetLR = false;
+				leftToRightPlaying = false;
+			}
+		}
+
+		if(Input.GetKey(KeyCode.K) || rightToLeftPlaying == true){
+			if(startingPosSetRL == false){
+				this.transform.position = leftToRightPoints[1];
+				startingPosSetRL = true;
+				transform.eulerAngles = new Vector2(0, 180);
+			}
+			rightToLeftPlaying=true;
+			transform.position = Vector3.MoveTowards(this.transform.position, leftToRightPoints[0], Time.deltaTime*8f);
+
+			
+			if(transform.position == leftToRightPoints[0]){
+				startingPosSetRL = false;
+				rightToLeftPlaying = false;
+			}
+		}
+
+		if (Input.GetKey (KeyCode.N) || leftToRightBINDLEPlaying == true) {
+			if(startingPosSetLRB == false){
+				this.transform.position = leftToRightBINDLEPoints[0];
+				startingPosSetLRB = true;
+				transform.eulerAngles = new Vector2 (0, 0);
+			}
+			if(transform.position.x < leftToRightBINDLEPoints[1].x){
+				leftToRightBINDLEPlaying = true;
+				transform.position = Vector3.MoveTowards(this.transform.position, leftToRightBINDLEPoints[1], Time.deltaTime*8f);
+			}
+			if(transform.position == leftToRightBINDLEPoints[1]){
+				animator.SetBool(charID + "ItemDrop", true);
+				itemDroppingTimer -= Time.deltaTime;
+			}
+
+			if(itemDroppingTimer < 0){
+				itemDroppingTimer -= Time.deltaTime;
+				animator.SetBool(charID + "ItemDrop", false);
+				transform.position = Vector3.MoveTowards(this.transform.position, leftToRightBINDLEPoints[2], Time.deltaTime*8f);
+			}
+
+			if(transform.position == leftToRightBINDLEPoints[2]){
+				itemDroppingTimer = 10;
+				startingPosSetLRB = false;
+				leftToRightBINDLEPlaying = false;
+			}
+		}
+
+
+		/*if (Input.GetKey (inputRight)) { //moving character right
 			transform.Translate (speed * Time.deltaTime, 0.0f, 0.0f);
 			transform.eulerAngles = new Vector2 (0, 0);
 		}
@@ -44,7 +111,18 @@ public class Character : MonoBehaviour {
 		if (Input.GetKey (inputLeft)) {//move character left
 			transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
 			transform.eulerAngles = new Vector2(0, 180);
-		}
+		}*/
+	}
+
+	void updateAnimationPoints(){
+		leftToRightPoints = new List<Vector3> ();
+		leftToRightPoints.Add (new Vector3 (-21f, -17f, 0f));
+		leftToRightPoints.Add (new Vector3 (21.85f, -17f, 0f));
+
+		leftToRightBINDLEPoints = new List<Vector3> ();
+		leftToRightBINDLEPoints.Add(new Vector3 (-21f, -17f, 0f));
+		leftToRightBINDLEPoints.Add(new Vector3 (-4.2f, -17f, 0f));
+		leftToRightBINDLEPoints.Add (new Vector3 (21.85f, -17f, 0f));
 	}
 
 
---
+++ HouseProj/Assets/Scripts/animating/Waypoint.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Developer: Tegan Scott
+ * Date: November 16, 2015
+ * Class: IMD 4902, Carleton University
+ * 
+ * Purpose: On the waypoint objects to send them to the next waypoint. This is a copy from the Unity 3D tutorial
+ * 
+ * */
+using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+	//this is a Unity function which detects when an object enters this object's trigger collider
+	void OnTriggerEnter(Collider hit){ // hit is the collider of the object which enters this trigger
+		if(hit.CompareTag("character")){ //check the tag of the object to see if it is the enemy, if so...
+			hit.GetComponent<AI>().NextWaypoint();// have the enemy move towards the next waypoint
+		}
+	}
+}
